dda1a51cb646b5fce3a74e7d9d901301a67e721e
yudifind <ydfind@ydfind.com>
2018-08-09 18:22:51 +0800

    简单的maven的示例：jar和war例子

---
+++ maven/mavendemo/src/main/java/com/ydfind/demo/maven/MvnDemo.java
@@ -0,0 +1,11 @@
+package com.ydfind.demo.maven;
+
+public class MvnDemo {
+	private static void hello() {
+		System.out.println("this is a test!");
+	}
+	
+	public static void main(String[] args) {
+		hello();
+	}
+}
